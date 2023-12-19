8675cab1c1df93f82ce1f732d55b77f27a874501
srinivas <vasumayeee.99@gmail.com>
2023-12-19 16:33:32 +0530

    log

--- 11-logs.sh
+++ 11-logs.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 ID=$(id -u)
 echo "script name $0 "
+timestamp=$(date +%F-%H-%M-%S)
+logfile="/tmp/$0-$timestamp.log"
 validate(){
     if [ $1 -ne 0 ]
     then 
@@ -18,7 +20,7 @@ then
 else
     echo "You are the root user"
 fi
-yum install mysql -y
+yum install mysql -y &>> $logfile
 validate $? "Mysql"
-yum install git -y
+yum install git -y &>> $logfile
 validate $? "Git"
