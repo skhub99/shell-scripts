47b3a65014dd58b581e856be3fb6069add77d7c5
srinivas <vasumayeee.99@gmail.com>
2023-12-19 10:42:12 +0530

    input names

---
+++ 4-variables.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+person1=$1
+person2=$2
+echo "$person1 : Hello $person2!"
+echo "$person2 : Heyy $person1!"
+echo "$person1 : good morning, how are you $person2?"
+echo "$person2 : good morning, i am good, how are you $person1 ?"
