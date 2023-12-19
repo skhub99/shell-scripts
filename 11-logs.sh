b83aa2847f5f0d4a5c39d53470a1e154e1c6acf2
srinivas <vasumayeee.99@gmail.com>
2023-12-19 16:23:17 +0530

    log

---
+++ 11-logs.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+ID=$(id -u)
+echo "script name $0 "
+validate(){
+    if [ $1 -ne 0 ]
+    then 
+        echo "Error-The installation of $2 is failed"
+        exit 1
+    else
+        echo "The installation of $2 is success"
+    fi
+}
+
+if [ $ID -ne 0 ]
+then 
+    echo "Please run the script with root user"
+    exit 1
+else
+    echo "You are the root user"
+fi
+yum install mysql -y
+validate $? "Mysql"
+yum install git -y
+validate $? "Git"
