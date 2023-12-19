0d057189cbe5eee6470ad15b146804f86ca6df82
srinivas <vasumayeee.99@gmail.com>
2023-12-19 11:22:58 +0530

    array

--- 7-array.sh
+++ 7-array.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
 fruits=("apple" "banana" "promo")
-echo "first value : ${fruits[0]}"
-echo "second value : ${fruits[1]}"
-echo "third value : ${fruits[2]}"
+echo "The given fruits are : ${fruits[@]}"
