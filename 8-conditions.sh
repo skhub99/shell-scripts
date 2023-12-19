a3909da5cc46b9d1a8e77e832ade633520510115
srinivas <vasumayeee.99@gmail.com>
2023-12-19 13:21:03 +0530

    conditions

--- 8-conditions.sh
+++ 8-conditions.sh
@@ -2,7 +2,7 @@
 Number=$1
 if [ $Number -gt 100 ]
 then
-    echo " The given number: $Number is greater than 100 "
+    echo " The given number is: $Number And is greater than 100 "
 else
-    echo " The given number: $Number is not greater than 100 "
+    echo " The given number is: $Number And is not greater than 100 "
 fi
