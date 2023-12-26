989af728515e664817587936fab2b3c7d1691d6f
srinivas <vasumayeee.99@gmail.com>
2023-12-26 08:11:09 +0530

    dis usage

---
+++ 17-disk-usage.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+dis_usage=$(df -hT | grep -vE )
+disk_threshold=1
+message=""
+while IFS= read line
+do
+    usage=$(echo $line | awk '{print $6F}' | cut -d % -f1)
+    partition=$(echo $line | awk '{print $1F}')
+    if [ $usage -ge $disk_threshold ]
+    then 
+        message+="high disk usage on $partition: $usage\n"
+    fi
+done <<< $disk_usage
+echo "message: $message"
